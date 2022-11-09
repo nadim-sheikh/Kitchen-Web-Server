f623e9ed76c401c7e4c4aa98157114166b478fe9
nadim-sheikh <rifatdesign07@gmail.com>
2022-11-09 20:18:56 +0600

    3nd commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
@@ -14,14 +14,40 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const foodCollection = client.db("rifat-kitchen").collection('food-item');
+
+        app.get('/limitFoodItem', async(req, res)=>{
+            const cursor = foodCollection.find({});
+            const limitFood = await cursor.limit(3).toArray();
+            res.send(limitFood);
+        })
+        app.get('/FoodItem', async(req, res)=>{
+            const cursor = foodCollection.find({});
+            const food = await cursor.toArray();
+            res.send(food);
+        })
+        app.get('/FoodItem/:id', async(req, res)=>{
+            const {id} = req.params;
+            const query = {_id: ObjectId(id)};
+            const food = await foodCollection.findOne(query);
+            res.send(food);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Kitchen Server Is Running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Kitchen Server Is Running On ${port}`);
 })
